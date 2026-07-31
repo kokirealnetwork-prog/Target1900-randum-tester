5adbf13fa2b305b8d48b5fb9b500a362bc865eff
cy24078-kokiosawa <koki.realnetwork@gmail.com>
2026-07-31 17:57:08 +0900

    Fix home logo alignment and spacing

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -72,7 +72,7 @@ export function HomeClient({ initialConfig }: { initialConfig: QuizConfig }) {
               src="/brand-yikes.svg"
               alt="tango randum tester"
               width={260}
-              height={23}
+              height={31}
               priority
             />
           </h1>
